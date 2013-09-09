7810c68ecf7eae70625fd6ba653ff45a2e5e9ff2
Slowking770 <mez613@gmail.com>
2013-09-09 12:12:29 +0300

    Started working on YartzhiehtAccess
    Added basic STABS

--- VirtGabbai/DataAccess/YarthzietAccess.cs
+++ VirtGabbai/DataAccess/YarthzietAccess.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using DataTypes;
+using DataCache;
 
 namespace DataAccess
 {
-    public class YarthzietAccess
+    public static class YarthzietAccess
     {
         /*todo 
          * 1) get all
          * 2) get specific
          * 3) map to type that is recognized in project
          */
+
+        public static Yarthzieht GetYarthzieht() { return null; }
+
+        public static List<Yarthzieht> GetYarthziehts() { return null; }
+
+        public static List<Yarthzieht> GetYarthziehts(string s) { return null; }
+
+        private static Yarthzieht ConvertToLocalType(zera_leviEntities ze) 
+        {
+            return null;
+        }
     }
 }
--- VirtGabbai/DataTypes/Yarthzieht.cs
+++ VirtGabbai/DataTypes/Yarthzieht.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace DataTypes
 {
-    public class Class1
+    public class Yarthzieht
     {
     }
 }
