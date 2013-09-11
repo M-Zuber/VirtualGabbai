e2c2b68b47def0953bad306c433cdd57fac5def2
Slowking770 <mez613@gmail.com>
2013-09-11 18:45:56 +0300

    Added lookup methods for updating Yarhtziets

--- VirtGabbai/DataAccess/YarthzietAccess.cs
+++ VirtGabbai/DataAccess/YarthzietAccess.cs
@@ -61,6 +61,46 @@ namespace DataAccess
             return this.ConvertMultipleYarhtzietToLocalType(test.t_yarthziehts.ToList<t_yarthziehts>());
         }
 
+        private t_yarthziehts LookupSpecificYarthzieht(int personId, DateTime date, string personName)
+        {
+            return (from test in Cache.CacheData.t_yarthziehts
+                    where test.person_id == personId &&
+                        test.date == date &&
+                        test.deceaseds_name == personName
+                    select test).First();
+        }
+
+        private List<t_yarthziehts> LookupYarhtzietsByName(int personId, string personName)
+        {
+            return (from test in Cache.CacheData.t_yarthziehts
+                    where test.deceaseds_name == personName
+                    select test).ToList<t_yarthziehts>();
+        }
+
+        private List<t_yarthziehts> LookupYarhtzietsByDate(int personId, DateTime date)
+        {
+            return (from test in Cache.CacheData.t_yarthziehts
+                    where test.date == date
+                    select test).ToList<t_yarthziehts>();
+        }
+
+        private List<t_yarthziehts> LookupYarhtzietsByRelation(int personId, string relation)
+        {
+            return (from test in Cache.CacheData.t_yarthziehts
+                    where test.relation == relation
+                    select test).ToList<t_yarthziehts>();
+        }
+
+        private List<t_yarthziehts> LookupAllYarthziehts(int personId)
+        {
+            return ((t_people)Cache.CacheData.t_people.Take(1)).t_yarthziehts.ToList<t_yarthziehts>();
+
+            //List<t_yarthziehts> AllYarhtziets = (from CurrentYarhtziet  in Cache.CacheData.t_yarthziehts
+            //                                     where CurrentYarhtziet.person_id == personId
+            //                                     select CurrentYarhtziet).ToList<t_yarthziehts>();
+
+        }
+
         #endregion
 
         #region Write
