1d99161144913d8f83326f914562ea369c37f5bf
Slowking770 <mez613@gmail.com>
2013-10-18 12:26:13 +0300

    Added enum for crud method codes

---
+++ VirtGabbai/Framework/Enums.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Framework
+{
+    public static class Enums
+    {
+        public enum CRUDResults
+        {
+            READ_SUCCESS,
+            READ_FAIL,
+            CREATE_SUCCESS,
+            CREATE_FAIL,
+            UPDATE_SUCCESS,
+            UPDATE_FAIL,
+            DELETE_SUCCESS,
+            DELETE_FAIL
+        }
+    }
+}
