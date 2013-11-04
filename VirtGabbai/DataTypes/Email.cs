9c77bc7b3918a33b94f40768556e9f66e39eaba3
Slowking770 <mez613@gmail.com>
2013-11-04 07:40:45 +0200

    Added skeleton class for Person and Email

---
+++ VirtGabbai/DataTypes/Email.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataTypes
+{
+    public class Email
+    {
+        #region Properties
+
+        public string Name { get; set; }
+
+        public string Domain { get; set; }
+
+        public string Suffix { get; set; }
+
+        #endregion
+
+        #region C'tor
+        
+        #endregion
+
+        #region Object Methods
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        #endregion
+    }
+}
---
+++ VirtGabbai/DataTypes/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataTypes
+{
+    public class Person
+    {
+        #region Properties
+
+        public int _Id { get; set; }
+
+        public Email Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public Account PersonalAccount { get; set; }
+
+        public List<PhoneNumber> PhoneNumbers { get; set; }
+
+        public List<Yahrtzieht> Yahrtziehts { get; set; }
+
+        #endregion
+
+        #region C'tor
+        
+        #endregion
+
+        #region Object Methods
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        #endregion
+    }
+}
