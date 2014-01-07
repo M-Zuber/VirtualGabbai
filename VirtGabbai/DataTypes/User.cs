d738d6774e9b9534f059c10ca29b123dc5131158
Slowking770 <mez613@gmail.com>
2014-01-07 18:26:40 +0200

    Added skeleton for User
    The unit tests are only skeletons, as are the methods in the class
    itself

---
+++ VirtGabbai/DataTypes/User.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Mail;
+
+namespace LocalTypes
+{
+    public class User
+    {
+        #region Properties
+
+        public int _Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public MailAddress Email { get; set; }
+
+        public PrivilegesGroup UserGroup { get; set; }
+
+        #endregion
+
+        #region C'tor
+
+        public User(int _id, string userName, string password, string email, PrivilegesGroup userGroup)
+        {
+            this._Id = _id;
+            this.UserName = userName;
+            this.Password = password;
+            this.Email = new MailAddress(email);
+            this.UserGroup = userGroup;
+        }
+
+        #endregion
+
+        #region Object Methods
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        #endregion
+    }
+}
---
+++ VirtGabbai/DataTypesTest/UserTest.cs
@@ -0,0 +1,243 @@
+﻿using LocalTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace LocalTypesTest
+{
+    
+    
+    /// <summary>
+    ///This is a test class for UserTest and is intended
+    ///to contain all UserTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class UserTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+        #region Setup
+
+        private static int id = 1;
+        private static string userName = "mez613";
+        private static string password = "*******";
+        private static string email = "jack@jingle.high";
+        private static PrivilegesGroup userGroup = new PrivilegesGroup(1, "admin",
+            new List<Privilege>()
+            {
+                new Privilege(1,"privilege:1"),
+                new Privilege(2,"privilege:2")
+            }
+        );
+
+        private User target = new User(id, userName, password, email, userGroup);
+
+        #endregion
+
+        #region Equals Tests
+
+        /// <summary>
+        ///A test for Equals
+        ///</summary>
+        [TestMethod()]
+        public void AllSameEqualsTest()
+        {
+            int _id = 0;
+            string userName = string.Empty;
+            string password = string.Empty;
+            string email = string.Empty;
+            PrivilegesGroup userGroup = null;
+            User target = new User(_id, userName, password, email, userGroup);
+            object obj = null;
+            bool expected = false;
+            bool actual;
+            actual = target.Equals(obj);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for Equals
+        ///</summary>
+        [TestMethod()]
+        public void AllDiffEqualsTest()
+        {
+            int _id = 0;
+            string userName = string.Empty;
+            string password = string.Empty;
+            string email = string.Empty;
+            PrivilegesGroup userGroup = null;
+            User target = new User(_id, userName, password, email, userGroup);
+            object obj = null;
+            bool expected = false;
+            bool actual;
+            actual = target.Equals(obj);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for Equals
+        ///</summary>
+        [TestMethod()]
+        public void DiffIdEqualsTest()
+        {
+            int _id = 0;
+            string userName = string.Empty;
+            string password = string.Empty;
+            string email = string.Empty;
+            PrivilegesGroup userGroup = null;
+            User target = new User(_id, userName, password, email, userGroup);
+            object obj = null;
+            bool expected = false;
+            bool actual;
+            actual = target.Equals(obj);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for Equals
+        ///</summary>
+        [TestMethod()]
+        public void DiffUserNameEqualsTest()
+        {
+            int _id = 0;
+            string userName = string.Empty;
+            string password = string.Empty;
+            string email = string.Empty;
+            PrivilegesGroup userGroup = null;
+            User target = new User(_id, userName, password, email, userGroup);
+            object obj = null;
+            bool expected = false;
+            bool actual;
+            actual = target.Equals(obj);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for Equals
+        ///</summary>
+        [TestMethod()]
+        public void DiffPasswordEqualsTest()
+        {
+            int _id = 0;
+            string userName = string.Empty;
+            string password = string.Empty;
+            string email = string.Empty;
+            PrivilegesGroup userGroup = null;
+            User target = new User(_id, userName, password, email, userGroup);
+            object obj = null;
+            bool expected = false;
+            bool actual;
+            actual = target.Equals(obj);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for Equals
+        ///</summary>
+        [TestMethod()]
+        public void DiffEmailEqualsTest()
+        {
+            int _id = 0;
+            string userName = string.Empty;
+            string password = string.Empty;
+            string email = string.Empty;
+            PrivilegesGroup userGroup = null;
+            User target = new User(_id, userName, password, email, userGroup);
+            object obj = null;
+            bool expected = false;
+            bool actual;
+            actual = target.Equals(obj);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for Equals
+        ///</summary>
+        [TestMethod()]
+        public void DiffUserGroupEqualsTest()
+        {
+            int _id = 0;
+            string userName = string.Empty;
+            string password = string.Empty;
+            string email = string.Empty;
+            PrivilegesGroup userGroup = null;
+            User target = new User(_id, userName, password, email, userGroup);
+            object obj = null;
+            bool expected = false;
+            bool actual;
+            actual = target.Equals(obj);
+            Assert.AreEqual(expected, actual);
+        }
+        #endregion
+
+        #region ToString Tests
+
+        /// <summary>
+        ///A test for ToString
+        ///</summary>
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            int _id = 0; // TODO: Initialize to an appropriate value
+            string userName = string.Empty; // TODO: Initialize to an appropriate value
+            string password = string.Empty; // TODO: Initialize to an appropriate value
+            string email = string.Empty; // TODO: Initialize to an appropriate value
+            PrivilegesGroup userGroup = null; // TODO: Initialize to an appropriate value
+            User target = new User(_id, userName, password, email, userGroup); // TODO: Initialize to an appropriate value
+            string expected = string.Empty; // TODO: Initialize to an appropriate value
+            string actual;
+            actual = target.ToString();
+            Assert.AreEqual(expected, actual);
+        }
+        
+        #endregion
+    }
+}
