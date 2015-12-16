3ff3cf629811ebf4bf91dc2625940af27287309a
Mordechai Zuber <mez613@gmail.com>
2015-12-16 06:31:55 +0200

    rename files
    next time dont be so quick to commit?

---
+++ VirtGabbai/Integration Tests/DataAccess.IntegrationTests/AccountRepositoryTests.cs
@@ -0,0 +1,35 @@
+﻿using DataAccess.IntegrationTests.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.IntegrationTests
+{
+    [TestClass()]
+    public class AccountRepositoryTests
+    {
+        VGTestContext _ctx = new VGTestContext();
+        AccountRepository repository;
+
+        [ClassInitialize()]
+        public void ClassSetup()
+        {
+            repository = new AccountRepository(_ctx);   
+        }
+
+        [TestCleanup()]
+        public void Cleanup()
+        {
+            _ctx.Database.Delete();
+        }
+
+        [TestMethod]
+        public void Exists_Null_Item_Returns_False()
+        {
+
+        }
+    }
+}
---
+++ VirtGabbai/Integration Tests/DataAccess.IntegrationTests/Helpers/VGTestContext.cs
@@ -0,0 +1,21 @@
+﻿using DataCache.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.IntegrationTests.Helpers
+{
+    public class VGTestContext : ZeraLeviContext
+    {
+        public VGTestContext():this("name=Database")
+        {
+
+        }
+
+        public VGTestContext(string nameOrConnectionString) : base(nameOrConnectionString)
+        {
+        }
+    }
+}
