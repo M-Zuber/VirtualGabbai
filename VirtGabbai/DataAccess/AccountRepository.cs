2930b9729d93dab749532f9b19d674e30846e9bf
Mordechai Zuber <mez613@gmail.com>
2015-12-17 07:39:59 +0200

    Make AccountRepository readonly

--- VirtGabbai/DataAccess/AccountRepository.cs
+++ VirtGabbai/DataAccess/AccountRepository.cs
@@ -9,7 +9,7 @@ using System.Data.Entity;
 
 namespace DataAccess
 {
-    public class AccountRepository : IFullAccessRepository<Account>
+    public class AccountRepository : IReadOnlyRepository<Account>
     {
         private ZeraLeviContext _context;
 
@@ -27,51 +27,5 @@ namespace DataAccess
         public IEnumerable<Account> Get() => Entities;
 
         public Account GetByID(int id) => Entities.FirstOrDefault(a => a.ID == id);
-
-        public void Add(Account item)
-        {
-            if (ValidateItem(item))
-            {
-                Entities.Add(item);
-                _context.SaveChanges();
-            }
-        }
-
-        public void Delete(Account item)
-        {
-            if (Exists(item))
-            {
-                Entities.Remove(item);
-                _context.SaveChanges();
-            }
-        }
-
-        public void Save(Account item)
-        {
-            if (ValidateItem(item))
-            {
-                var current = GetByID(item.ID);
-
-                if (current == null)
-                {
-                    current = new Account() { Person = item.Person };
-                    Entities.Add(current);
-
-                    // Update the item passed in, in case the caller does something with it
-                    // THINK: should it just return current?
-                    item.ID = current.ID;
-                    item.PersonID = current.PersonID;
-                }
-
-                current.Donations = item.Donations;
-                current.LastMonthlyPaymentDate = item.LastMonthlyPaymentDate;
-                current.MonthlyPaymentAmount = item.MonthlyPaymentAmount;
-
-                item.Person = current.Person;
-                _context.SaveChanges();
-            }
-        }
-
-        public bool ValidateItem(Account item) => item != null && item.Person != null && item.PersonID > 0;
     }
 }
